0b4e95052c87d5a0eea4380793a37ab3833eb8f6
MisturaO <misturaojulari91@gmail.com>
2023-06-22 21:32:41 +0100

    Removed explicit null 'True' on first and last names

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User:
     Updated to DBStorage from file storage"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 from models.place import Place
 
@@ -10,11 +11,11 @@ from models.place import Place
 class User(BaseModel, Base):
     """This class defines the table 'users' by variouss
     attributes(columns) for the database storage"""
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
     """Relationship is created with the place
     table(i.e. the Place class model)"""
