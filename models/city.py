1b8bd6f4d796a0d493d847f58bbfdb30ceab44b2
MisturaO <misturaojulari91@gmail.com>
2023-06-22 21:24:49 +0100

    Imported declarative base function

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
@@ -9,7 +10,7 @@ from models.place import Place
 class City(BaseModel, Base):
     """ The city class, contains state ID and name.
         Updated to DBStorage from file storage """
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     # state = Relationship('State',, back_populates='cities')
