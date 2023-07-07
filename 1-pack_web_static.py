ff2104873cfdd5da2383ab6cca1280f729ecd165
Dev-wonderful <adeonederful20@gmail.com>
2023-07-07 16:06:21 +0000

    packing

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,12 +4,14 @@ import os
 from datetime import datetime
 from fabric.api import *
 
+
 def do_pack():
     """pack the files"""
     date = datetime.now().strftime('%Y%m%d%H%M%S')
-    if  not os.path.isdir('versions'):
+    if not os.path.isdir('versions'):
         parent_dir = os.getcwd()
         full_path = os.path.join(parent_dir, 'versions')
         os.makedirs(full_path)
-    local('tar -cvfz /versions/web_static_{}.tgz web_static'.format(date))
-
+    cmd = 'tar -cvfz /versions/web_static_{}.tgz web_static'.format(date)
+    if local('{}'.format(cmd)).failed:
+        return None
