4741a44f1a29ac086a5c9959690b3f11301ce72e
Dev-wonderful <adeonederful20@gmail.com>
2023-07-07 01:05:32 +0000

    mod

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,13 +14,12 @@ fi
 if [[ ! -d /data/web_static/shared ]]; then
         mkdir /data/web_static/shared
 fi
-if [[ ! -d /data/web_static/current  ]]; then
-        mkdir /data/web_static/current
-        ln -fs /data/web_static/current /data/web_static/releases/test
-        echo 'Hello World!' > /data/web_static/current/index.html
-fi
-if [[ -d /data/web_static/current ]]; then
-        ln -fs /data/web_static/current /data/web_static/releases/test
+if [[ ! -d /data/web_static/releases/test  ]]; then
+        mkdir /data/web_static/releases/test
+        echo 'Hello World!' > /data/web_static/releases/test/index.html
+        ln -fs /data/web_static/releases/test /data/web_static/current
+elif [[ -d /data/web_static/current ]]; then
+        ln -fs /data/web_static/releases/test /data/web_static/current
 fi
 chown -RL ubuntu:ubuntu /data
 ADD='\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}'
