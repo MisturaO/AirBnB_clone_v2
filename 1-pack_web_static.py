43561f064c55869b757ffb1043a045a2eae686d2
Dev-wonderful <adeonederful20@gmail.com>
2023-07-07 15:50:11 +0000

    packing

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """A fabfile to zip web_static folder"""
 import os
-import datetime
+from datetime import datetime
 from fabric.api import *
 
 def do_pack():
     """pack the files"""
     date = datetime.now().strftime('%Y%m%d%H%M%S')
-    if !os.path.isdir('versions'):
-        parent_dir = os.cwd()
+    if  not os.path.isdir('versions'):
+        parent_dir = os.getcwd()
         full_path = os.path.join(parent_dir, 'versions')
-        os.mkdir(path)
-    local('tar -cvfz versions/web_static_{}.tgz web_static'.format(full_path))
+        os.makedirs(full_path)
+    local('tar -cvfz /versions/web_static_{}.tgz web_static'.format(date))
 
