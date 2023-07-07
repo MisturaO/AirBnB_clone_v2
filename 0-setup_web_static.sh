a2094b450307135cb280837342c2e7aea1198377
Dev-wonderful <adeonederful20@gmail.com>
2023-07-07 01:06:28 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,32 +2,27 @@
 # script that sets up my web servers for the deployment of web_static
 apt-get -y update
 apt-get -y install nginx
-if [[ ! -d '/data' ]]; then
-	mkdir '/data'
+if [[ ! -d /data ]]; then
+        mkdir /data
 fi
-if [[ ! -d '/data/web_static' ]]; then
-	mkdir '/data/web-static'
+if [[ ! -d /data/web_static ]]; then
+        mkdir /data/web_static
 fi
-if [[ ! -d '/data/web_static/releases' ]]; then
-	mkdir '/data/web_static/releases'
+if [[ ! -d /data/web_static/releases ]]; then
+        mkdir /data/web_static/releases
 fi
-if [[ ! -d '/data/web_static/shared' ]]; then
-	mkdir '/data/web_static/shared'
+if [[ ! -d /data/web_static/shared ]]; then
+        mkdir /data/web_static/shared
 fi
-if [[ ! -d '/data/web_static/current'  ]]; then
-	mkdir '/data/web_static/current'
-	ln -fs '/data/web_static/current' '/data/web_static/releases/test'
-	echo 'Hello World!' > /data/web_static/current/index.html
+if [[ ! -d /data/web_static/current  ]]; then
+        mkdir /data/web_static/current
+        ln -fs /data/web_static/current /data/web_static/releases/test
+        echo 'Hello World!' > /data/web_static/current/index.html
 fi
-if [[ -d '/data/web_static/current' ]]; then
-	ln -fs '/data/web_static/current' '/data/web_static/releases/test'
+if [[ -d /data/web_static/current ]]; then
+        ln -fs /data/web_static/current /data/web_static/releases/test
 fi
 chown -RL ubuntu:ubuntu /data
-ADD=$(cat << EOF 
-	location /hbnb_static/ {
-		alias /data/web_static/current/
-	}
-EOF
-)
-sed -i ".server_name _;.a $ADD" /etc/nginx/sites-available/default
+ADD='\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}'
+sed -i "/server_name _;/a $ADD" /etc/nginx/sites-available/default
 service nginx restart
