95ed5c0454326513a97e133f3fde6921f16eab0f
Dev-wonderful <adeonederful20@gmail.com>
2023-07-07 22:31:37 +0000

    pack it all

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,10 +8,16 @@ from fabric.api import *
 def do_pack():
     """pack the files"""
     date = datetime.now().strftime('%Y%m%d%H%M%S')
+    parent_dir = os.getcwd()
+    full_path = os.path.join(parent_dir, 'versions')
     if not os.path.isdir('versions'):
-        parent_dir = os.getcwd()
-        full_path = os.path.join(parent_dir, 'versions')
         os.makedirs(full_path)
-    cmd = 'tar -cvzf /versions/web_static_{}.tgz web_static'.format(date)
-    if local('{}'.format(cmd)).failed:
+    archived_file = 'web_static_{}.tgz'.format(date)
+    cmd = 'tar -cvzf /versions/{} web_static'.format(archived_file)
+    result = local('{}'.format(cmd))
+    if result.return_code == 0:
+        size = os.path.getsize(os.path.join(full_path, archived_file))
+        print('web_static packed: versions/{} -> {}Bytes'.format(archived_file,
+                                                                 size))
+    else:
         return None
