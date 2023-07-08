54db31926261b8be702073cc10b8a8e58825570f
Dev-wonderful <adeonederful20@gmail.com>
2023-07-08 08:20:11 +0100

    commas

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,13 +3,13 @@
 exec {'update':
   provider => shell,
   command  => 'sudo apt -y update',
-  before   => Exec['install nginx']
+  before   => Exec['install nginx'],
 }
 
 exec {'install nginx':
   provider => shell,
   command  => 'sudo apt -y install nginx',
-  require  => Exec['update']
+  require  => Exec['update'],
 }
 
 file {'/data':
@@ -22,32 +22,32 @@ file {'/data/web_static':
     enable  => directory,
     owner   => 'ubuntu',
     group   => 'ubuntu',
-    require => File['/data']
+    require => File['/data'],
 }
 file {'/data/web_static/shared':
     enable  => directory,
     owner   => 'ubuntu',
     group   => 'ubuntu',
-    require => File['/data/web_static']
+    require => File['/data/web_static'],
 }
 file {'/data/web_static/releases':
     enable  => directory,
     owner   => 'ubuntu',
     group   => 'ubuntu',
-    require => File['/data/web_static']
+    require => File['/data/web_static'],
 }
 file {'/data/web_static/releases/test':
     enable  => directory,
     owner   => 'ubuntu',
     group   => 'ubuntu',
-    require => File['/data/web_static/releases']
+    require => File['/data/web_static/releases'],
 }
 file {'/data/web_static/releases/test/index.html':
     enable  => file,
     owner   => 'ubuntu',
     group   => 'ubuntu',
-    content => 'Hello Web Static'
-    require => File['/data/web_static/releases/test']
+    content => 'Hello Web Static',
+    require => File['/data/web_static/releases/test'],
 }
 file {'/data/web_static/current':
     enable => link,
@@ -59,11 +59,11 @@ add = "\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
 exec{'location hbnb_static':
   provider => shell,
   command  => "sudo sed -i '/server_name _;/a ${add};' /etc/nginx/sites-available/default",
-  require  => Exec['install nginx']
+  require  => Exec['install nginx'],
 }
 # restart nginx after config  update
 exec {'restart':
   provider => shell,
   command  => 'sudo service nginx restart',
-  require  => Exec['location hbnb_static']
+  require  => Exec['location hbnb_static'],
 }
