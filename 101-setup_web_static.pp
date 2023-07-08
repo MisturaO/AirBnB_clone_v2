0d8f416f34148499645d914646727e8e2784bd57
Dev-wonderful <adeonederful20@gmail.com>
2023-07-08 08:09:57 +0100

    fixing puppet syntax and require

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -12,39 +12,44 @@ exec {'install nginx':
   require  => Exec['update']
 }
 
-file {'/data'
+file {'/data':
     enable => directory,
     owner  => 'ubuntu',
     group  => 'ubuntu',
 }
 
-file {'/data/web_static'
+file {'/data/web_static':
     enable => directory,
     owner  => 'ubuntu',
     group  => 'ubuntu',
+    require => File['/data']
 }
-file {'/data/web_static/shared'
+file {'/data/web_static/shared':
     enable => directory,
     owner  => 'ubuntu',
     group  => 'ubuntu',
+    require => File['/data/web_static']
 }
-file {'/data/web_static/releases'
+file {'/data/web_static/releases':
     enable => directory,
     owner  => 'ubuntu',
     group  => 'ubuntu',
+    require => File['/data/web_static']
 }
-file {'/data/web_static/releases/test'
+file {'/data/web_static/releases/test':
     enable => directory,
     owner  => 'ubuntu',
     group  => 'ubuntu',
+    require => File['/data/web_static/releases']
 }
-file {'/data/web_static/releases/test/index.html'
+file {'/data/web_static/releases/test/index.html':
     enable  => file,
     owner   => 'ubuntu',
     group   => 'ubuntu',
     content => 'Hello Web Static'
+    require => File['/data/web_static/releases/test']
 }
-file {'/data/web_static/current'
+file {'/data/web_static/current':
     enable => link,
     target => '/data/web_static/releases/test'
     owner  => 'ubuntu',
@@ -54,9 +59,11 @@ add = "\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
 exec{'location hbnb_static':
   provider => shell,
   command  => "sudo sed -i '/server_name _;/a ${add};' /etc/nginx/sites-available/default",
+  require => Exec['install nginx']
 }
 # restart nginx after config  update
 exec {'restart':
   provider => shell,
   command  => 'sudo service nginx restart',
+  require => Exec['location hbnb_static']
 }
