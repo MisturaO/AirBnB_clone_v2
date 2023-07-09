c27952e98da3e11cb550a4b67848023e5572a08a
MisturaO <misturaojulari91@gmail.com>
2023-07-09 13:35:17 +0100

    Compress before sending to server

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,10 +11,10 @@ def do_pack():
     to be transfered to the server as a compressed file"""
     file = 'web_static_'
     dt_time = datetime.utcnow().strftime('%Y%m%D%H%M%S')
-    file_name = file + dt_time + '.tgz'
+    file_name = file + dt_time + ".tgz"
 
     if os.path.isdir('versions') is False:
-        if local('mkdir versions').failed is True:
+        if local('mkdir -p versions').failed is True:
             return None
     if local('tar -cvzf versions/{} web_static'
              .format(file_name)).failed is True:
