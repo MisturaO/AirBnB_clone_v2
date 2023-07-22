948fa6da43b4b5b49dbf93cb5368bddfd82bf8f7
MisturaO <misturaojulari91@gmail.com>
2023-07-22 11:43:56 +0100

    starts flask app with 3 routes and route param

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,9 +10,13 @@ Route 2:
     '/hbnb'
 Displays:
     “HBNB”
+Route 3:
+    '/c/<text>'
+    Displays “C ” followed by the value of the <text>
+    variable (replace underscore _ symbols with a space )
 """
 
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -28,11 +32,20 @@ def dispaly_hbnb():
     """Dispalys 'HBNB!'"""
     return 'HBNB'
 
+ 
+def remove_url_underscore(text):
+    """
+    Helper function for route '/c/<text>' that will replace
+    underscores(_) in the url param with spaces
+    """
+    return text.replace("_", " ")
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_C_var_value(text):
     """Dispays 'C' followed by URL parameters(<text>) value"""
-    return f'C {text}'
+    formatted_url_output = remove_url_underscore(text)
+    return f'C {formatted_url_output}'
 
 
 if __name__ == "__main__":
