15db8df69839fb76c296659b86c83d7e55c87d11
MisturaO <misturaojulari91@gmail.com>
2023-07-23 17:11:02 +0100

    Improve storage

--- models/amenity.py
+++ models/amenity.py
@@ -10,5 +10,3 @@ class Amenity(BaseModel, Base):
     """Defines what each amenity would have"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity,
-                                   viewonly=False, back_populates='amenities')
