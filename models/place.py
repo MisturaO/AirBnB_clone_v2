cf82796daad8349aaf40bbfbdda943e2db83a4ce
MisturaO <misturaojulari91@gmail.com>
2023-07-23 17:11:42 +0100

    Improve storage

--- models/place.py
+++ models/place.py
@@ -37,8 +37,7 @@ class Place(BaseModel, Base):
         reviews = relationship("Review", cascade='all, delete-orphan',
                                backref="place")
         amenities = relationship('Amenity', secondary=place_amenity,
-                                 back_populates='place_amenities',
-                                 viewonly=False)
+                                 backref='places', viewonly=False)
     else:
         @property
         def reviews(self):
